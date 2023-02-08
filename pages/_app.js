ba932b4c1f2fd7c07205ced939f3504f538d97cf
AngelinaLD <gelya_lebedeva_2019@mail.ru>
2023-02-08 22:49:09 +0300

    ver 3

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import "./styles.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 /** @format */
-
+import React, { Component } from "react";
 function HomePage() {
-  return <div>Инна писька</div>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Инна писька на голубом фоне
+    </h1>
+  );
 }
 
 export default HomePage;
